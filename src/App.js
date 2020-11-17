75eb320bacb416b0fc16acb0aac3c8b4c4c3a4f2
SakifKhan98 <sakifkhan98@gmail.com>
2020-11-17 19:12:43 +0600

    Added Dashboard to main Project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Shared/Header/Header';
-import HeaderMain from './Components/Home/HeaderMain/HeaderMain';
+import { createContext, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard/Dashboard";
+import AddRentHouse from "./Components/Dashboard/AddRentHouse/AddRentHouse";
+import BookingList from "./Components/Dashboard/BookingList/BookingList";
+import MyRent from "./Components/Dashboard/MyRent/MyRent";
+import Home from "./Components/Home/Home/Home";
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div>
-     <Header></Header>
-     <HeaderMain></HeaderMain>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/addRentHouse">
+            <AddRentHouse />
+          </Route>
+          <Route path="/bookingList">
+            <BookingList />
+          </Route>
+          <Route path="/myRent">
+            <MyRent />
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../Shared/Header/Header";
+import HeaderMain from "../HeaderMain/HeaderMain";
+
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+      <HeaderMain></HeaderMain>
+    </div>
+  );
+};
+
+export default Home;
